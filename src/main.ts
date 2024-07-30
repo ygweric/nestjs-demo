3c10cc1dae7f2f3fc1bb49763fae2909a8d0b19b
yanggw <yanggw@glodon.com>
2024-07-30 09:19:32 +0800

    update port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3100;
+  console.log(`using port : ${port}`);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
